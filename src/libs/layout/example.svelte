71fc66f462b4762f931968bc740874daa9a15d9f
Thomas Collardeau <thomas@collardeau.com>
2019-10-08 14:16:44 +0100

    first layout implementation

---
+++ src/libs/layout/example.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Layout from "./index.svelte";
+
+  let classes = {
+    header: "h-12",
+    content: "bg-gray-200",
+    footer: "bg-gray-400"
+  };
+</script>
+
+<Layout {classes}>
+  <header slot="header">My Header</header>
+  <section slot="content">My Content</section>
+  <aside slot="left">Left Section</aside>
+  <aside slot="right">Right Section</aside>
+  <footer slot="footer">My Footer</footer>
+</Layout>
---
+++ src/libs/layout/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let classes = {};
+  $: contentClass = classes.content || "";
+  $: headerClass = classes.header || "";
+  $: footerClass = classes.footer || "";
+</script>
+
+<div class="min-h-screen flex flex-col">
+  <div class={headerClass}>
+    <slot name="header" />
+  </div>
+  <div class="{contentClass} flex-1">
+    <slot name="content" />
+  </div>
+  <div class={footerClass}>
+    <slot name="footer" />
+  </div>
+</div>
