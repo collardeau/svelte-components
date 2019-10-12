ee79060c859a9e0537b010787f300c873968abde
Thomas Collardeau <thomas@collardeau.com>
2019-10-12 15:28:58 +0100

    work on fluid layout

--- src/libs/fluid-layout/example.svelte
+++ src/libs/fluid-layout/example.svelte
@@ -1,47 +1,38 @@
 <script>
   import Layout from "./index.svelte";
-
-  let breakpoint = "lg";
 </script>
 
 <style>
+  /* target size specific styles: */
+
+  :global(.svelte-fluid-layout-large main) {
+    width: 66%;
+  }
+  :global(.svelte-fluid-layout-small section) {
+    padding: 10px 0;
+  }
+
+  /* your app styles: */
+
   :global(body) {
-    margin: 0;
+    margin: 0; /* reset */
   }
 
   header,
   footer {
-    height: 60px;
     background-color: lightblue;
+    height: 60px;
   }
   section,
   aside {
     background-color: lightslategray;
   }
-
-  :global(.fluid-layout-small section) {
-    height: 100px;
-  }
-  :global(.fluid-layout-small aside) {
-    height: 50px;
-  }
-
-  :global(.fluid-layout-large main) {
-    width: 66%;
-  }
-  :global(.fluid-layout-large section, .fluid-layout-large aside) {
-    min-height: 100%;
-  }
-
-  :global(.flex-1) {
-    background-color: pink;
-  }
 </style>
 
-<Layout {breakpoint}>
+<Layout breakpoint="md">
   <header slot="header">Header</header>
-  <section slot="menu">Menu</section>
-  <main slot="main">Content</main>
-  <aside slot="aside">Aside</aside>
+  <section slot="side1">Side 1</section>
+  <main slot="main">Main</main>
+  <aside slot="side2">Side 2</aside>
   <footer slot="footer">Footer</footer>
 </Layout>
